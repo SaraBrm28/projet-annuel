38986ddb52b1bd45ee80adbe60c05374c96b1f53
SaraBrm28 <168735302+SaraBrm28@users.noreply.github.com>
2025-03-08 13:49:11 +0100

    duplication div

--- src/Categorie.tsx
+++ src/Categorie.tsx
@@ -6,6 +6,9 @@ const Categorie = () => {
     <div>
         <div className='text-8xl text-white'>Bonjour!</div>
         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur culpa, iste sit nam nesciunt fugiat eum in at dolorem ipsum et debitis! Tempora fugiat unde molestiae sed aut quae? Quam?</p>
+   
+    <div className='text-8xl text-white'>Bonjour!</div>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur culpa, iste sit nam nesciunt fugiat eum in at dolorem ipsum et debitis! Tempora fugiat unde molestiae sed aut quae? Quam?</p>
     </div>
   )
 }
